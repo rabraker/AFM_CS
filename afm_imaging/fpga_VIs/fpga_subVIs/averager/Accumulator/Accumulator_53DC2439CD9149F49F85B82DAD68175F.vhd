0f25da472dc84921a7a3406a9c92160f781e022f
Roger Braker <abraker@gmail.com>
2017-07-03 11:23:12 -0600

    commit before branching to explore network stream and PC

---
+++ afm_imaging/fpga_VIs/fpga_subVIs/averager/Accumulator/Accumulator_53DC2439CD9149F49F85B82DAD68175F.vhd
@@ -0,0 +1,108 @@
+--------------------------------------------------------------------------------
+--    This file is owned and controlled by Xilinx and must be used solely     --
+--    for design, simulation, implementation and creation of design files     --
+--    limited to Xilinx devices or technologies. Use with non-Xilinx          --
+--    devices or technologies is expressly prohibited and immediately         --
+--    terminates your license.                                                --
+--                                                                            --
+--    XILINX IS PROVIDING THIS DESIGN, CODE, OR INFORMATION "AS IS" SOLELY    --
+--    FOR USE IN DEVELOPING PROGRAMS AND SOLUTIONS FOR XILINX DEVICES.  BY    --
+--    PROVIDING THIS DESIGN, CODE, OR INFORMATION AS ONE POSSIBLE             --
+--    IMPLEMENTATION OF THIS FEATURE, APPLICATION OR STANDARD, XILINX IS      --
+--    MAKING NO REPRESENTATION THAT THIS IMPLEMENTATION IS FREE FROM ANY      --
+--    CLAIMS OF INFRINGEMENT, AND YOU ARE RESPONSIBLE FOR OBTAINING ANY       --
+--    RIGHTS YOU MAY REQUIRE FOR YOUR IMPLEMENTATION.  XILINX EXPRESSLY       --
+--    DISCLAIMS ANY WARRANTY WHATSOEVER WITH RESPECT TO THE ADEQUACY OF THE   --
+--    IMPLEMENTATION, INCLUDING BUT NOT LIMITED TO ANY WARRANTIES OR          --
+--    REPRESENTATIONS THAT THIS IMPLEMENTATION IS FREE FROM CLAIMS OF         --
+--    INFRINGEMENT, IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A   --
+--    PARTICULAR PURPOSE.                                                     --
+--                                                                            --
+--    Xilinx products are not intended for use in life support appliances,    --
+--    devices, or systems.  Use in such applications are expressly            --
+--    prohibited.                                                             --
+--                                                                            --
+--    (c) Copyright 1995-2017 Xilinx, Inc.                                    --
+--    All rights reserved.                                                    --
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+-- You must compile the wrapper file Accumulator_53DC2439CD9149F49F85B82DAD68175F.vhd when simulating
+-- the core, Accumulator_53DC2439CD9149F49F85B82DAD68175F. When compiling the wrapper file, be sure to
+-- reference the XilinxCoreLib VHDL simulation library. For detailed
+-- instructions, please refer to the "CORE Generator Help".
+
+-- The synthesis directives "translate_off/translate_on" specified
+-- below are supported by Xilinx, Mentor Graphics and Synplicity
+-- synthesis tools. Ensure they are correct for your synthesis tool(s).
+
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+-- synthesis translate_off
+LIBRARY XilinxCoreLib;
+-- synthesis translate_on
+ENTITY Accumulator_53DC2439CD9149F49F85B82DAD68175F IS
+  PORT (
+    b : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
+    clk : IN STD_LOGIC;
+    c_in : IN STD_LOGIC;
+    ce : IN STD_LOGIC;
+    bypass : IN STD_LOGIC;
+    sclr : IN STD_LOGIC;
+    q : OUT STD_LOGIC_VECTOR(95 DOWNTO 0)
+  );
+END Accumulator_53DC2439CD9149F49F85B82DAD68175F;
+
+ARCHITECTURE Accumulator_53DC2439CD9149F49F85B82DAD68175F_a OF Accumulator_53DC2439CD9149F49F85B82DAD68175F IS
+-- synthesis translate_off
+COMPONENT wrapped_Accumulator_53DC2439CD9149F49F85B82DAD68175F
+  PORT (
+    b : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
+    clk : IN STD_LOGIC;
+    c_in : IN STD_LOGIC;
+    ce : IN STD_LOGIC;
+    bypass : IN STD_LOGIC;
+    sclr : IN STD_LOGIC;
+    q : OUT STD_LOGIC_VECTOR(95 DOWNTO 0)
+  );
+END COMPONENT;
+
+-- Configuration specification
+  FOR ALL : wrapped_Accumulator_53DC2439CD9149F49F85B82DAD68175F USE ENTITY XilinxCoreLib.c_accum_v11_0(behavioral)
+    GENERIC MAP (
+      c_add_mode => 0,
+      c_ainit_val => "0",
+      c_b_type => 0,
+      c_b_width => 32,
+      c_bypass_low => 0,
+      c_ce_overrides_sclr => 0,
+      c_has_bypass => 1,
+      c_has_c_in => 1,
+      c_has_ce => 1,
+      c_has_sclr => 1,
+      c_has_sinit => 0,
+      c_has_sset => 0,
+      c_implementation => 0,
+      c_latency => 12,
+      c_out_width => 96,
+      c_scale => 0,
+      c_sclr_overrides_sset => 1,
+      c_sinit_val => "0",
+      c_verbosity => 0,
+      c_xdevicefamily => "spartan6"
+    );
+-- synthesis translate_on
+BEGIN
+-- synthesis translate_off
+U0 : wrapped_Accumulator_53DC2439CD9149F49F85B82DAD68175F
+  PORT MAP (
+    b => b,
+    clk => clk,
+    c_in => c_in,
+    ce => ce,
+    bypass => bypass,
+    sclr => sclr,
+    q => q
+  );
+-- synthesis translate_on
+
+END Accumulator_53DC2439CD9149F49F85B82DAD68175F_a;
