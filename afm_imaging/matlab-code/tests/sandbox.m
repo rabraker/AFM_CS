f45480a2ec0bb2301883c2689232eb8ad58fdf56
Roger Braker <abraker@fastmail.com>
2017-07-12 13:49:20 -0600

    forgot one

---
+++ afm_imaging/matlab-code/tests/sandbox.m
@@ -0,0 +1,25 @@
+classdef sandbox < matlab.unittest.TestCase
+    %   Detailed explanation goes here
+    
+    
+    methods(Test)
+%         function test_MeasEntityMu(testCase)
+%             x0 = 0.3;
+%             y0 = -0.2;
+%             N = 4;
+%             ind = 1;
+%             xy_rate = [0.1, 0.2];
+%             me_mu = MeasEntityMu(x0, y0, N, ind, xy_rate);
+%             expected = [x0; y0; ind;
+%                 x0 + xy_rate(1)*1; y0 + xy_rate(2)*1; ind;
+%                 x0 + xy_rate(1)*2; y0 + xy_rate(2)*2; ind;
+%                 x0+xy_rate(1)*3; y0+xy_rate(2)*3;      -1];
+%             
+%             testCase.assertEqual(expected, me_mu.as_vector(),...
+%     'AbsTol', 1e-10)
+%             
+%         end
+    end
+    
+end
+
