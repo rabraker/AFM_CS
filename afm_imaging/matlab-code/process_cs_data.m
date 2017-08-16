f7b821bf504747c88b8140b8e10cfc91d518a050
arnold <abraker@fastmail.com>
2017-08-16 17:06:39 -0600

    cleanup process_cs_data.m

--- afm_imaging/matlab-code/process_cs_data.m
+++ afm_imaging/matlab-code/process_cs_data.m
@@ -5,6 +5,7 @@ width = 5;
 microns_per_volt = 50/10;
 pix_per_volt = (pix/width)*microns_per_volt;
 
+Ts = 40e-6;
 
 % This path should be set to the root of your github working directory. 
 % root = 'C:\Users\arnold\Documents\labview';  % For windows
@@ -39,10 +40,13 @@ for k = 1:max(dat_meas(:,end))
    E_ks = dat_meas(inds, 3);
    U_ks = dat_meas(inds, 5);
    
+
    % Convert the x and y voltage data into bined and averaged pixel coordinates.
    x_pix = floor(pixelize(X_ks, path_pix)*pix);
    y_pix = floor(pixelize(Y_ks, path_pix)*pix);
    
+   
+%    keyboard
    % Bin the height/deflection data into pixels and average. 
    e_pix = pixelize(E_ks, path_pix);
    u_pix = pixelize(U_ks, path_pix);
@@ -50,8 +54,8 @@ for k = 1:max(dat_meas(:,end))
    % Finally, collect in the kth cell array entry. 
   pix_dat{k} = [x_pix, y_pix, e_pix, u_pix];
 end
-%%
-close all
+%
+% close all
 f1 = figure(1);
 ylabel('u_z')
 xlabel('pixel')
@@ -101,45 +105,66 @@ for k = 1:length(pix_dat)
             pixelifsampled(n_row, m_col) = 1;
     end
     
-    
+
 end
 %%
-f2 = figure(2); clf
-ax2 = gca();
-imshow_sane(I, ax2, width, width);
-
+I = detrend_plane(I, pixelifsampled);
+% f2 = figure(2); clf
+% ax2 = gca();
+% 
+% imshow_sane(I, ax2, width, width);
+% 
 
 %%
+% Make the image square, to use smp.
+I = I(1:end-1, 19:end);
+size(I)
+pixelifsampled = pixelifsampled(1:end-1, 19:end);
+size(pixelifsampled)
+
+% ********* SMP *************
+reconstruct_root = fullfile(root, 'reconstruction/SMP');
+reconstruct_path = genpath(reconstruct_root)
+addpath(reconstruct_path)
+
+[n,m] = size(I);
+Ir_smp = SMP(I.*pixelifsampled,pixelifsampled,round(0.02*n*m));
+
 
 reconstruct_root = fullfile(root, 'reconstruction/BP');
 reconstruct_path = genpath(reconstruct_root)
 addpath(reconstruct_path)
 
+% ********* BP *************
 [n m] = size(I);
 
 I_vector = PixelMatrixToVector(I);
+
 pixelifsampled = PixelMatrixToVector(pixelifsampled);
 I_vector = I_vector(find(pixelifsampled>0.5));
 
 A = @(x) IDCTfun(x,pixelifsampled);
 At = @(x) DCTfun(x,pixelifsampled);
 
-Ir = idct(l1qc_logbarrier(At(I_vector), A, At, I_vector, 0.1));
-Ir = real(Ir);
-%%
+Ir_bp = idct(l1qc_logbarrier(At(I_vector), A, At, I_vector, 0.1));
+Ir_bp = real(Ir_bp);
+
 % close all;
-f3 = figure(3)
-subplot(1,2,1)
+f5 = figure(5)
+subplot(1,3,1)
 ax3 = gca();
 imshow_sane(I.*PixelVectorToMatrix(pixelifsampled,[n m]), ax3, width, width);
 title('sample');
 
-subplot(1,2,2)
+subplot(1,3,2)
 ax4 = gca();
-imshow_sane(PixelVectorToMatrix(Ir,[n m]), ax4, width, width);
+imshow_sane(PixelVectorToMatrix(Ir_bp,[n m]), ax4, width, width);
 title('BP reconstruction');
 
-
+subplot(1,3,3)
+ax5 = gca();
+imshow_sane(Ir_smp, ax5, width, width)
+title('SMP reconstruction');
 
 
 
