edddcd7a69676dedaf4ff8b95bc42d41f1051b6c
Roger Braker <abraker@fastmail.com>
2017-08-25 13:24:22 -0600

    minor edits

--- afm_imaging/matlab-code/process_raster_data_full.m
+++ afm_imaging/matlab-code/process_raster_data_full.m
@@ -12,8 +12,10 @@ parent_path = fullfile(dat_root, parent_name);
 datmat = csvread(dat_path);
 parent_dat = csvread(parent_path);
 
+xyref = reshape(parent_dat', 2, [])';
+xref = xyref(:,1);
 %%
-
+Ts = 40e-6;
 samps_per_period = size(parent_dat,1)/2; % twice as many in here for x & y.
 samps_per_line = samps_per_period/2;
 
@@ -42,7 +44,27 @@ figure(3)
 lo = min(min(I_fit));
 hi = max(max(I_fit));
 imshow(I_fit, [lo, hi])
+%%
+% visualize tracking error.
+np = 3;
+xx = datmat(:,1);
+x_np = xx(1:np*length(xref));
+x_np = x_np - min(x_np);
+figure(200); clf; hold on
+t = [0:1:length(xref)*np-1]'*Ts;
+xref_np = repmat(xref, np, 1);
+p1 = plot(t, xref_np*volts2micron);
+p1.DisplayName = '$x_{ref}$';
+p2 = plot(t, x_np*volts2micron);
+p2.DisplayName = '$x(k)$';
 
+ylm = ylim;
+ylim([0, ylm(2)+.1*ylm(2)])
+ylabel('x-dir [\mu m]')
+xlabel('time [s]')
+leg1 = legend([p1, p2]);
+set(leg1, 'FontSize', 14, 'interpreter', 'latex', 'orientation', 'horizontal')
+leg1.Position=[0.6436    0.8590    0.2611    0.0640];
 
 %% Now try by actually using the x-y measured data;
 
