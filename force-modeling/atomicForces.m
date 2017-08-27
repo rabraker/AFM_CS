eeece1234f8fa6ee7b4f94a7286585d5eea0fbc7
arnold <abraker@fastmail.com>
2017-08-26 20:00:45 -0600

    old work on force modeling

--- force-modeling/atomicForces.m
+++ force-modeling/atomicForces.m
@@ -1,6 +1,18 @@
 classdef atomicForces
-    %ATOMICFORCES Summary of this class goes here
-    %   Detailed explanation goes here
+% Taken from 
+% High Performance feedback for fast scanning atomic force microscopes,
+% G. Schitter, P. Menold, H.F. Knapp, F. Allgower, A. Stemmer, Rev. Sci.
+% Instruments, vol 72, no 8.
+%
+% k1 = fo * R
+% where
+% R = tip radius
+% fo = (2/3)pi^2 eps rho1 rho2 sig^2
+%
+% rho1 = number density of tip  (what does this mean ?)
+% rho2 = number density of sample
+% eps = minimum energy of lennard-jones potential
+% sig = spherical approximaton of the molecule diameter (tip or sample???)
     
     properties
         sigma;
