ec3492c7643653fd8927e60bd613eec656a5398c
arnold <abraker@fastmail.com>
2017-08-29 15:34:09 -0600

    add functions path

--- afm_imaging/matlab-code/acc_plot_cs_img.m
+++ afm_imaging/matlab-code/acc_plot_cs_img.m
@@ -1,6 +1,6 @@
 clc
 clear
-
+addpath('functions')
 data_root = fullfile(getdataroot, 'cs-data');
 cs_exp_data_name = 'cs-traj10-500_out_8-25-2017-14.csv';
 cs_exp_data_name = 'cs-traj10-500_8-22-2017_08.csv';
