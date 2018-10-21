0f76ec7b474db1918223eac0eb6bc44883605e71
arnold <abraker@fastmail.com>
2018-10-21 12:41:12 -0600

    use the CsExp class methods for reconstruction

--- matlab-code/process_cs_data_dynamic_detrend.m
+++ matlab-code/process_cs_data_dynamic_detrend.m
@@ -49,75 +49,51 @@ figs{1}= figure(1000); clf; hold on, grid on;
 figs{2}= figure(2000); clf; hold on, grid on;
 figs{3}= figure(3000); clf; hold on, grid on;
 
-cs_exp.process_cs_data(true, figs);
+cs_exp.process_cs_data(false, figs);
 
 %%
 pixelifsampled = cs_exp.pix_mask;
-I = cs_exp.Img;
+I = cs_exp.Img_raw;
 
 figure(100 + 10*i);
 ax = gca();
-imshow(I, [min(min(I)), max(max(I))])
+imshow(cs_exp.Img_raw, [min(cs_exp.Img_raw(:)), max(cs_exp.Img_raw(:))])
 
 
 bp = true;
 
 % ********* SMP *************
+%%
+clear CsExp
+cs_exp.solve_smp1d();
+%%
 
-[n,m] = size(I);
-
-% Ir_smp = SMP(I.*pixelifsampled,pixelifsampled,round(0.02*n*m));
-
-tic
-samp_frac = sum(sum(pixelifsampled))/(n*m)
-reduce_frac = 0.1;  %It's my understanding Yufan says 1/10 of sub-sample fraction.
-maxiter = round(samp_frac*reduce_frac*n*m)
-Ir_smp = SMP_1D(I, pixelifsampled, maxiter);
-time_smp = toc;
 
 f5 = figure(9 + 10*i); clf
 subplot(2,3,1)
 ax3 = gca();
-imshow_sane(I, ax3, cs_exp.width, cs_exp.width);
+imshow_sane(cs_exp.Img_raw, ax3, cs_exp.width, cs_exp.width);
 title('sample');
 
 subplot(2,3,3)
 ax5 = gca();
-imshow_sane(Ir_smp, ax5, cs_exp.width, cs_exp.width)
+imshow_sane(cs_exp.Img_smp1d, ax5, cs_exp.width, cs_exp.width)
 title('SMP reconstruction');
 drawnow
 
 if bp
-    % ********* BP *************
-    [n m] = size(I);
-    tic
-    I_vector = PixelMatrixToVector(I);
-
-    pixelifsampled_vec = PixelMatrixToVector(pixelifsampled);
-    I_vector = I_vector(find(pixelifsampled_vec>0.5));
-
-    A = @(x) IDCTfun(x,pixelifsampled_vec);
-    At = @(x) DCTfun(x,pixelifsampled_vec);
-
-    Ir_bp = idct(l1qc_logbarrier(At(I_vector), A, At, I_vector, 0.1));
-    Ir_bp = real(Ir_bp);
-    bp_im = PixelVectorToMatrix(Ir_bp,[n m]);
-    time_bp = toc;
-    fprintf('SMP Time: %f \nBP Time: %f\n', time_smp, time_bp);
-
-    bp_im = detrend_plane(bp_im);
-    
+    cs_exp.solve_basis_pursuit();
     subplot(2,3,2)
     ax4 = gca();
     % imshow_sane(PixelVectorToMatrix(Ir_bp,[n m]), ax4, cs_exp.width, cs_exp.width);
     
-    imshow_sane(bp_im, ax4, cs_exp.width, cs_exp.width);
+    imshow_sane(cs_exp.Img_bp, ax4, cs_exp.width, cs_exp.width);
 
     title('BP reconstruction');
 end
 %
 clc
-s = metadata2text(ExpMetaData, Ts)
+s = metadata2text(cs_exp.meta_exp, Ts)
 s = sprintf('%s\nperc=%.3f', s, sum(sum(pixelifsampled))/cs_exp.npix/cs_exp.npix);
 s2 = sprintf('%s', cs_exp_data_name)
 disp(s)
