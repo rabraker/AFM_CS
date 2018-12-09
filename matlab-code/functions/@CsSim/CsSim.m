eb1098068802752de70322acf835868afc060eba
arnold <abraker@fastmail.com>
2018-12-09 15:30:02 -0700

    Modify CsSim to have properties npix and sample_frac. Modify solve_bp method to optionally 2D dct.

--- matlab-code/functions/@CsSim/CsSim.m
+++ matlab-code/functions/@CsSim/CsSim.m
@@ -8,20 +8,27 @@ classdef CsSim < handle
     Img_sub_sampled;
     Img_bp
     Img_smp1d
-  
+    sample_frac;
+    npix;
   end
   
   methods 
     function self = CsSim(Img_original, pix_mask)
       self.Img_original = Img_original;
       self.pix_mask = pix_mask;
+      self.npix = size(pix_mask, 1);
+      self.sample_frac = sum(pix_mask(:))/self.npix^2;
       self.Img_sub_sampled = pix_mask.*Img_original;
     end
 
     
-    function solve_bp(self, recalc)
+    function solve_bp(self, recalc, use_2d)
+      
       if nargin <2
         recalc = false;
+      end
+      if nargin <3
+        use_2d = false;
       end
       if ~recalc && ~isempty(self.Img_bp) && sum(self.Img_bp(:)) ~= 0
         warning(['BP solution already calculated. Pass recalc flag ' ...
@@ -36,9 +43,12 @@ classdef CsSim < handle
       pix_mask_vec = PixelMatrixToVector(self.pix_mask);
       % y, set of measurements. have to remove all the spots we didn't sample.
       I_vector = I_vector(find(pix_mask_vec>0.5)); 
-
       A = @(x) IDCTfun(x,pix_mask_vec);
       At = @(x) DCTfun(x,pix_mask_vec);
+      if use_2d
+        A = @(x) IDCTfun_2D(x,pix_mask_vec, n);
+        At = @(x) DCTfun_2D(x,pix_mask_vec, n);
+      end
 
       Ir_bp = idct(l1qc_logbarrier(At(I_vector), A, At, I_vector, 0.1));
       Ir_bp = real(Ir_bp);
