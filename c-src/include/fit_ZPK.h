48ec76625ab7c235a8682f347b1b981362b34020
Roger Braker <abraker@fastmail.com>
2019-02-02 00:26:20 -0700

    Figured out how to make labview accept a struct in the DLL call. That seems like a better way to pass the results back.

--- c-src/include/fit_ZPK.h
+++ c-src/include/fit_ZPK.h
@@ -28,11 +28,33 @@
 #endif
 
 #include <complex.h>
+#include "mpfit.h"
+
+struct  FitSOSResult {
+  int stable_num;
+  int stable_den;
+  double bestnorm;
+  double orignorm;
+  int niter;
+  int nfev;
+  int mpfit_status;
+  int nfree;
+  int npegged;
+  int nfunc;
+
+};
+
 
 int absfunc(int m, int n, double *theta, double *resid, double **dvec, void *user_data);
 
 DLL_PUBLIC  int fit_sos(int Len_omegas, double *omegas, double *resp_real,
                         double *resp_imag, int len_theta, double *theta,
-                        double *result);
+                        double result[9]);
+
+DLL_PUBLIC int fit_sos_st(int N_omegas, double *omegas, double *resp_real,
+                          double *resp_imag, int N_theta, double *theta,
+                          struct  FitSOSResult *fit_sos_result);
+
+
 
 #endif
--- c-src/src/fit_ZPK.c
+++ c-src/src/fit_ZPK.c
@@ -3,9 +3,10 @@
 #include <math.h>
 
 #include "mpfit.h"
-// #include "fit_ZPK.h"
+#include "fit_ZPK.h"
 #include "SOS.h"
 #include "plotting.h"
+
 
 
 int absfunc(int m, int n, double *theta, double *resid, double **dvec, void *user_data)
@@ -138,3 +139,57 @@ int fit_sos(int N_omegas, double *omegas, double *resp_real,
 
 
 
+
+int fit_sos_st(int N_omegas, double *omegas, double *resp_real,
+               double *resp_imag, int N_theta, double *theta,  struct FitSOSResult *fit_sos_result){
+  int status=0, i=0;
+
+  mp_result *mpres = malloc(sizeof(mp_result));
+  double complex *resp_fit = malloc(N_omegas * sizeof( double complex));
+  double complex *resp = malloc(N_omegas * sizeof( double complex));
+
+  mpres->bestnorm=0;
+  mpres->orignorm=0;
+  mpres->niter=0;
+  mpres->nfev=0;
+  mpres->status=0;
+  mpres->npar=0;
+  mpres->nfree=0;
+  mpres->npegged=0;
+  mpres->nfunc=0;
+  mpres->resid=0;
+  mpres->xerror=0;
+  mpres->covar=0;
+
+
+  for (i=0; i < N_omegas; i++){
+    resp[i] = resp_real[i] + resp_imag[i]*I;
+  }
+
+
+  FRF_Data frd = {.N = N_omegas,
+                  .Resp=resp,
+                  .omegas = omegas,
+                  .Ts=40e-6};
+
+  status = mpfit(&absfunc, N_omegas, N_theta, theta, 0, 0, &frd, mpres);
+
+  fit_sos_result->stable_num =  poly2_is_z_stable(1.0, theta[1], theta[2]);  // Numerator is stable?
+  fit_sos_result->stable_den =  poly2_is_z_stable(1.0, theta[3], theta[4]); // Denominator is stable?
+  fit_sos_result->bestnorm = mpres->bestnorm;
+  fit_sos_result->orignorm = mpres->orignorm;
+  fit_sos_result->niter = mpres->niter;
+  fit_sos_result->nfev = mpres->nfev;
+  fit_sos_result->mpfit_status = mpres->status;
+  fit_sos_result->npegged = mpres->npegged;
+  fit_sos_result->nfunc = mpres->nfunc;
+
+  // result[9] = poly2_is_z_stable(1.0, theta[1], theta[2]); // Numerator is stable?
+  // result[10] = poly2_is_z_stable(1.0, theta[3], theta[4]); // Denominator is stable?
+  // free(mpres);
+  free(resp_fit);
+  free(resp);
+
+  return status;
+
+}
