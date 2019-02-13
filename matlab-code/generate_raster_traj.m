7231016c0ba2b0e606868f2fc87c2a50ef5f7c45
Roger Braker <abraker@fastmail.com>
2019-02-13 15:54:06 -0700

    Initial implementation of raster scanning as CS scan.

--- matlab-code/generate_raster_traj.m
+++ matlab-code/generate_raster_traj.m
@@ -30,7 +30,7 @@ Ki_x = 0.01;
 % Trace is one line at sec_line. The whole period is trace and re-trace.
 
 
-raster_freq = .25; % Hz.
+raster_freq = 1; % Hz.
 raster_period = 1/raster_freq;
 
 image_side = 5; % micro-meters.
@@ -41,12 +41,11 @@ raster_amplitude = image_side/2;
 
 % resolution, ie, how many lines?
 %  (eventually, this should be the same as as pixels)
-npix = 512;
+npix = 128;
 square_num_lines = npix;
 y_height = (1/square_num_lines)*image_side;
 
-meta = struct('raster_freq', raster_freq, 'npix', npix,...
-              'width', image_side);
+
 
 
 [x_rasterdata, truefreq, points_per_line] = raster(raster_freq, Ts, raster_period-Ts, 'coerce', 1,...
@@ -73,6 +72,7 @@ plot(y_rasterdata.Time, y_rasterdata.Data);
 % [x(1), y(1), x(2), y(2), ....]
 
 xy_data = zeros(2*length(x_rasterdata.Time), 1);
+
 j = 1;
 for k=1:2:length(xy_data)
     xy_data(k) = x_rasterdata.Data(j)*AFM.mic2volt_xy;
@@ -80,17 +80,43 @@ for k=1:2:length(xy_data)
     j = j+1;
 end
 
+%%
+% scan_type: 0=raster, 1=CS
+
+            
 % write it to a .csv file
 
 data_name = sprintf('raster_scan_%dpix_%dmic_%.2dHz.csv',npix,image_side, raster_freq)
-%
+
 target_dir = sprintf('%dmicrons', image_side)
-%%
+%
 data_root = PATHS.raster_image_data('5microns', 'parents')
 % fullfile(getdataroot, 'raster', target_dir);
 % if exist(data_root, 'file') ~=2
 %     mkdir(fullfile(getdataroot, 'raster'), target_dir)
 % end
+
+data_in_path = fullfile(data_root, data_name)
+meta_path = strrep(data_in_path, '.csv', '.json');
+
+csvwrite(data_in_path, xy_data);
+opts.FileName = meta_path;
+            
+savejson('', struct('raster_freq', raster_freq, 'npix', npix,...
+              'width', image_side,...
+              'points_per_line', int64(length(x_rasterdata.Time)/2),...
+              'points_per_period', int64(length(x_rasterdata.Time)),...
+              'total_num_points', int64(npix*length(x_rasterdata.Time)),...
+              'scan_type', 0,  'fpga_input', xy_data(:)'), opts);
+
+
+
+
+
+
+
+%%
+% OLD METHOD, READING .MAT FILE IN LABVIEW
 data_in_path = fullfile(data_root, data_name)
 meta_path = strrep(data_in_path, '.csv', '.mat');
 
