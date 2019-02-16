1d606dd64ebf68fe590ab3dcb5e6ce1a6d62cc58
Roger Braker <abraker@fastmail.com>
2019-02-16 11:27:26 -0700

    Add legends to plot_all_cycles.m

--- matlab-code/functions/@CsExp/plot_all_cycles.m
+++ matlab-code/functions/@CsExp/plot_all_cycles.m
@@ -89,7 +89,7 @@ function [hands] = plot_all_cycles(self, ax1, ax2, ax3, ax4)
   % figs = {Fig_uz, Fig_ze, Fig_x, Fig_y};
   % axs = {ax1, ax2, ax3, ax4};        
 
-  indc = {'k',        'r', [0, .75, .75], 'b', [.93 .69 .13], ;
+  indc = {   'k',        'r',   [0, .75, .75],       'b',        [.93 .69 .13], ;
           'xy-move', 'tip down', 'tip settle',  '$\mu$-path scan', 'tip up',};
 
 
@@ -98,7 +98,10 @@ function [hands] = plot_all_cycles(self, ax1, ax2, ax3, ax4)
   Num_cycles = min([length(self.idx_state_s.move), length(self.idx_state_s.tdown),...
                     length(self.idx_state_s.tsettle), length(self.idx_state_s.scan),...
                     length(self.idx_state_s.tup)]);
-
+h_x = gobjects(length(state_seq),1);
+h_y = gobjects(length(state_seq),1);
+h_ze = gobjects(length(state_seq),1);
+h_uz = gobjects(length(state_seq),1);
   for idx_cs_seq = 1:Num_cycles
 
     for k=1:length(state_seq)
@@ -111,22 +114,40 @@ function [hands] = plot_all_cycles(self, ax1, ax2, ax3, ax4)
       t_k  = idx_state*self.Ts;
       if plotuz
         uz_k = self.uz(idx_state);
-        plot(ax1, t_k, uz_k, 'color', indc{1, k});
+        h_uz(k) = plot(ax1, t_k, uz_k, 'color', indc{1, k});
+        h_uz(k).DisplayName = indc{2,k};
       end
       if plotze
         ze_k = self.ze(idx_state);
-        plot(ax2, t_k, ze_k, 'color', indc{1, k});
+        h_ze(k) = plot(ax2, t_k, ze_k, 'color', indc{1, k});
+        h_ze(k).DisplayName = indc{2,k};
       end
       if plotx
         x_k = self.x(idx_state);
-        plot(ax3, t_k, x_k, 'color', indc{1, k});
+        h_x(k) = plot(ax3, t_k, x_k, 'color', indc{1, k});
+        h_x(k).DisplayName = indc{2,k};
       end
       if ploty
         y_k = self.y(idx_state);
-        plot(ax4, t_k, y_k, 'color', indc{1, k});
+        h_y(k) = plot(ax4, t_k, y_k, 'color', indc{1, k});
+        h_y(k).DisplayName = indc{2,k};
       end
     end
 
+  end
+
+  if plotuz
+    legend(h_uz);
+  end
+
+  if plotze
+    legend(h_ze);
+  end
+  if plotx
+    legend(h_x);
+  end
+  if ploty
+    legend(h_y);
   end
 
   plot(ax2, [self.t(1), self.t(end)], [.05, .05], '--k')
