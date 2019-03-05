2f0ea4cf1a77c3d0b9d6a95a93ff16728c821527
arnold (zeus) <abraker@fastmail.com>
2019-03-04 19:38:43 -0700

    Build a new class and script to generate the zbounce data, using the json scheme.

---
+++ matlab-code/functions/MuPathBounce.m
@@ -0,0 +1,67 @@
+
+
+classdef MuPathBounce < handle
+  properties
+    N_paths;
+    Ts=AFM.Ts;
+    
+    XR_volt_starts;
+    YR_volt_starts;
+    samps_per_bounce;
+  end
+  methods
+    function self = MuPathBounce(N_paths, samps_per_bounce)
+      self.N_paths = N_paths;
+      self.XR_volt_starts = zeros(N_paths, 1);
+      self.YR_volt_starts = zeros(N_paths, 1);
+      self.samps_per_bounce = samps_per_bounce;
+    end
+    function write_data_json(self, json_fname)
+      vec = self.as_vector();
+      % For full compatibility with doing raster as CS, we need extra fields.
+      % scan_type: 0=raster, 1=CS
+      data = struct(...
+              'raster_freq', 0,...
+              'npix', 0,...
+              'width', 0,...
+              'points_per_line', int64(0),...
+              'points_per_period', int64(0),...
+              'total_num_points', int64(length(vec)/3),... % /3 because have x,y,idx
+              'number_of_scans', self.N_paths,...
+              'scan_type', 1,...
+              'mu_length', 0,...
+              'overscan_samples', 0,...
+              'pre_scan_samples', 0,...
+              'tip_velocity', 0,...
+              'actual_sub_samble_perc', 0,...
+              'fpga_input', vec(:)',...
+              'pix_idx', int64([0,0])'...
+              );
+    
+     opts.FileName = json_fname;
+     savejson('', data, opts)
+
+    end
+    
+    function vec = as_vector(self)
+      vec = [];
+      % The easiest way to interleve the data suitible for pumping into the
+      % FPGA is to build a matrix and then reshape.
+      for k=1:self.N_paths
+        xt = zeros(self.samps_per_bounce,1);
+        yt = xt;
+        met_idx = xt*0+k;
+
+        % the setpoint has a meta-idx=0;
+        vec_k = [xt(1);
+                 yt(1);
+                 0];
+        
+        met_idx(end) = -1;
+        vec_k = [vec_k, [xt(:)'; yt(:)'; met_idx(:)']];  %#ok<AGROW>
+        vec = [vec; reshape(vec_k, [], 1)];              %#ok<AGROW>
+      end
+      
+    end
+  end
+end
--- matlab-code/generate_mu_path_zbounce.m
+++ matlab-code/generate_mu_path_zbounce.m
@@ -7,114 +7,19 @@ clear, clc
 
 addpath('classes')
 addpath('functions')
-
+clc
 %-------------- Location of System Model -------------------------
-dataroot      = fullfile(getMatPath(), 'AFM_SS',...
-                'System_Identification', 'data', 'data_xaxis'); 
-expName           = ['22-Jun-2016_exp01'];
-modFitName    = [expName, '.mat'];
-modFitPath    = fullfile(dataroot, modFitName);
-load(modFitPath, 'modelFit')
+samps_per_path = 5000;
+mpb = MuPathBounce(5, samps_per_path)
 
 
-% FitNum    = 'FitNum001';
-PLANT_init_x = ltiFit(modFitPath, 'SS02').sys;
-%-------------- Define scan Params -------------------------
-
-Ts = 40e-6;
-TsTicks = 1600;
-Ki_x = 0.01;
-
-if 1
-    width = 5;  % microns
-    pix =512;  % image resolution.
-    mu_length = .5;  % 500 nm. length of the horizontal mu-path. 
-    sub_sample_frac = 0.20;  % Percent of pixels to subsample. 
-end
 % Unit conversions.
-pix_per_micron = pix/width;
-mu_pix = ceil(mu_length*pix_per_micron);
 
-% ************************************************
-% ************************************************
+clc
+fname = sprintf('cs-traj-z-bounce_%dsamples.json', samps_per_path);
+fname = fullfile(PATHS.exp, 'z-bounce', 'parents', fname)
 
-Ts = 40e-6;  % AFM sample rate. 
-Fs = 1/Ts;
-microns2volts = 1/5;  %10/50;
-raster_freq = 1;  % hz
-raster_period = 1/raster_freq;
-
-microns_per_second = width/(raster_period/2);
-pixels_per_second = pix_per_micron * microns_per_second;
-volts_per_second = microns_per_second*microns2volts;
-volts_per_sample = volts_per_second * Ts;  % Ts = seconds per sample
-
-% Convert mu-path length in pixels to volts
-mu_micron = (1/pix_per_micron) * mu_pix;
-mu_volts = mu_micron * microns2volts;
-mu_Nsamples = ceil(mu_volts / volts_per_sample);
-
-ME = MeasEntityMu.factory([volts_per_sample*0, 0]);
-
-N_mve = 1;
-% ************************************************
-
-XR = zeros(100,1);
-YR = XR;
-
-% '------------------correct by overscan for ramp ramp set ---------------
-N = mu_Nsamples;
-x_rate = volts_per_sample*0;
+mpb.write_data_json(fname)
 
 
-N_extra = 500;
-mu_Nsamples_extra = N+N_extra;
-meta_cell = repmat({mu_Nsamples_extra}, 1, length(XR));
-MT = MasterTrajster(XR*0, YR*0, meta_cell, MoveEntityStatic.factory(N_mve), ME);
-
-
-
-MT.visualize_sampling;
-
-xlabel('x [v]')
-ylabel('y [v]')
-grid on
-
-fname = 'cs-traj-z-bounce_500nmequiv.csv';
-
-% create meta file name
-% target_dir = sprintf('%dmicrons/parents', width)
-if ispc
-  data_root = 'Z:\afm-cs\z-bounce\parents';
-else
-  data_root = '/media/labserver/afm-cs/z-bounce/parents';
-end
-
-meta_in = strrep(fname, '.csv', '.mat');
-meta_data_path = fullfile(data_root, meta_in);
-
-%%
-
-
-tip_velocity = width/(0.5*(1/raster_freq));
-
-CsExpMetaIn.width = width;
-CsExpMetaIn.nom_perc = sub_sample_frac;
-CsExpMetaIn.mu_length = mu_length;
-CsExpMetaIn.tip_velocity = tip_velocity;
-CsExpMetaIn.npix = pix
-CsExpMetaIn.pixelifsampled = 0; %pixelifsampled;
-CsExpMetaIn.actual_perc = 0; %perc;
-
-datafile = fullfile(data_root, fname)
-
-if 1
-    MT.write_csv(datafile)
-    save(meta_data_path, 'CsExpMetaIn')
-end
-   
-
-
-
-    
 
