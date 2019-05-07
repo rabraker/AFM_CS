dffc5bb667ae630e2f696e1ecfba0d0e6ab6ef5b
arnold (zeus) <abraker@fastmail.com>
2019-05-06 21:44:09 -0600

    Dont detrend lines when computing metric.

--- matlab-code/functions/ssim_psnr_norm.m
+++ matlab-code/functions/ssim_psnr_norm.m
@@ -1,14 +1,12 @@
 function [psn, ssm] = ssim_psnr_norm(Im_master, Im_compare, varargin)
-  Im_master = detrend_lines_local(Im_master);
-  Im_compare = detrend_lines_local(Im_compare);
+%   Im_master = detrend_lines_local(Im_master);
+%   Im_compare = detrend_lines_local(Im_compare);
   
   if length(varargin) > 1
     DRng = varargin{1};
   else
     Im_master = Im_master - mean(Im_master(:));
     Im_compare = Im_compare - mean(Im_compare(:));
-    %   Im_master = Im_master - min(Im_master(:));
-    %   Im_compare = Im_compare - min(Im_compare(:));
     
     mx1 = max((Im_master(:)));
     mx2 = max((Im_compare(:)));
