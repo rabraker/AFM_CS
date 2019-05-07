aabae9873b2a0c4f12336949dbcafcdde4a0002d
arnold (zeus) <abraker@fastmail.com>
2019-05-06 21:30:41 -0600

    Use latest data for plotting cs cycle.

--- matlab-code/publish_plotting/plot_cs_cycle.m
+++ matlab-code/publish_plotting/plot_cs_cycle.m
@@ -21,20 +21,13 @@ gg = zpk(z(end-1:end), p(1:2), 1, G.Ts);
 hole_depth = (20);
 
 chan_map = ChannelMap([1:5]);
-exp_date = '3-20-2019'
+exp_date = '4-26-2019'
 % ----------------------- Load and Process CS-data -----------------------------
 dat_root = PATHS.raster_image_data(size_dir, exp_date);
 
 % ----------------------- Load and Process raster-data -------------------------
 cs_files = {...
-'cs-traj-512pix-10perc-500nm-5mic-01Hz-250prescan-notconnect_out_3-20-2019-01.csv',...
-'cs-traj-512pix-10perc-500nm-5mic-02Hz-250prescan-notconnect_out_3-20-2019-01.csv',...
-'cs-traj-512pix-10perc-500nm-5mic-04Hz-250prescan-notconnect_out_3-20-2019-01.csv',...
-'cs-traj-512pix-10perc-500nm-5mic-05Hz-250prescan-notconnect_out_3-20-2019-01.csv',...
-'cs-traj-512pix-15perc-500nm-5mic-01Hz-250prescan-notconnect_out_3-20-2019-01.csv',...
-'cs-traj-512pix-15perc-500nm-5mic-02Hz-250prescan-notconnect_out_3-20-2019-01.csv',...
-'cs-traj-512pix-15perc-500nm-5mic-04Hz-250prescan-notconnect_out_3-20-2019-01.csv',...
-'cs-traj-512pix-15perc-500nm-5mic-05Hz-250prescan-notconnect_out_3-20-2019-01.csv',...
+'cs-traj-512pix-10perc-500nm-5mic-01Hz-150prescan-notconnect_out_4-26-2019-01.csv',...
 };
 
 
@@ -49,6 +42,15 @@ for k=1:length(cs_files)
   cs_exps{k}.print_state_times();
   cs_exps{k}.sub_sample_frac()
 end
+% taken from cycle 3, where tip detaches.
+cs_exp1 = cs_exps{1};
+zdfl_free = -0.637;
+cs_exp1.ze = cs_exp1.ze - zdfl_free;
+
+
+cs_exp1.x = cs_exp1.x*AFM.volts2mic_xy;
+cs_exp1.y = cs_exp1.y*AFM.volts2mic_xy;
+cs_exp1.uz = cs_exp1.uz*AFM.volts2nm_z;
 
 %%
 [~, axs] = make_cs_traj_figs(figbase, 4);
@@ -57,7 +59,7 @@ cs_exps{1}.plot_all_cycles(axs{1:4});
 
 
 %%
-cs_exp1 = cs_exps{1};
+
 indc = {   'k',        'r',   [0, .75, .75],       'b',        [.93 .69 .13], 'm';
           'xy-move', 'tip down', 'tip settle',  '$\mu$-path scan', 'tip up', 'connect'};
         
@@ -65,29 +67,39 @@ Fig = mkfig(100, 7, 7); clf
 ha = tight_subplot(4, 1, [.05, .05], [0.075, .03], [.08, .01], false);
 
 
-
-CS_idx1 = cs_exps{1}.find_cycle_idx(16.31);
+tstart = 14.9;
+CS_idx1 = cs_exps{1}.find_cycle_idx(tstart);
 CS_idx2 = CS_idx1 + 3;
 
 %
 % The Bad Case
-traj_names = {'ze', 'y'};
+
 state_names = {'move', 'tdown', 'tsettle', 'scan', 'tup'};
 
 % The Good Case
 traj_names = {'y', 'x', 'uz', 'ze'};
-names = {'$Y$', '$X$', '$u_Z$', 'deflection'};
-xlm1 = [16.25, 16.5];
+names = {'$Y$ [$\mu$m]', '$X$ [$\mu$m]', '$u_Z [nm]$', 'deflection [v]'};
+
+
+
+xlm1 = [14.91, 15.1];
+hands = gobjects(length(state_names), 1);
 for j=1:length(traj_names)
   h_j = ha(j);
   for k=1:length(state_names)
     state_name = state_names{k};
-    idx_move = cs_exp1.plot_traj_from_csidx_by_state(CS_idx1, CS_idx2,...
-      state_name, traj_names{j}, h_j, 0, 'color', indc{1, k});
+    h = cs_exp1.plot_traj_from_csidx_by_state(CS_idx1, CS_idx2,...
+        state_name, traj_names{j}, h_j, 0, 'color', indc{1, k});
+    h.DisplayName = indc{2, k};
+    if strcmp(traj_names{j}, 'x')
+        hands(k) = h;
+    end
+    
   end
+  
   xlim(h_j, xlm1);
   grid(h_j, 'on')
-  ylabel(h_j, [names{j} ' [v]'], 'FontSize', 12);
+  ylabel(h_j, [names{j} ], 'FontSize', 12);
   if j<3
     set(h_j, 'XTickLabel', [])
   else
@@ -95,7 +107,8 @@ for j=1:length(traj_names)
   end
 end
 
+leg = legend(hands, 'Position', [0.0933 0.6311 0.1543 0.1000]);
 %%
-save_fig(Fig, 'notes/figures/cs_cycle')
+
 
 save_fig(Fig, fullfile(PATHS.thesis_root, 'plots-afm-cs-final/figures/cs_cycle'))
