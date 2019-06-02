c4143f307750dddca5786824516a575f7b9397a0
Roger Braker <abraker@fastmail.com>
2019-06-01 18:51:08 -0600

    Look at psd of scans, and detrend, to see if we stay in 1pixel.

--- matlab-code/test_DxDy.m
+++ matlab-code/test_DxDy.m
@@ -23,15 +23,16 @@ hole_depth = (20);
 chan_map = ChannelMap([1:5]);
 exp_date = '5-31-2019'
 % ----------------------- Load and Process CS-data -----------------------------
-
+%%
 cs_files = {...
-'cs-traj-512pix-3perc-500nm-5mic-01Hz-150prescan-notconnect_out_5-31-2019-01.csv',...
+%'cs-traj-512pix-3perc-500nm-5mic-01Hz-150prescan-notconnect_out_6-1-2019-01.csv',...
+'cs-traj-512pix-3perc-500nm-5mic-01Hz-150prescan-notconnect_out_6-1-2019-06.csv',...
 };
 
-data_root = '/media/labserver/afm-cs/z-bounce/5-31-2019/';
+% data_root = '/media/labserver/afm-cs/z-bounce/5-31-2019/';
+data_root = 'Z:\afm-cs\z-bounce\6-1-2019';
 
 
-%%
 use_ze = false;
 % data_root = PATHS.cs_image_data(size_dir, exp_date);
 cs_exps = {};
@@ -45,10 +46,29 @@ for k=1:length(cs_files)
   cs_exps{k}.print_state_times();
   cs_exps{k}.sub_sample_frac()
 end
-%%
-
-% [~, axs] = make_cs_traj_figs(1, 2);
-% cs_exps{1}.plot_all_cycles([], [], axs{3:4}, [], 512)
-%%
+%
+if 0
+[~, axs] = make_cs_traj_figs(1, 2);
+cs_exps{1}.plot_all_cycles([], [], axs{:}, [], 512)
+%
 [~, axs2] = make_cs_traj_figs(102, 2);
-cs_exps{1}.plot_all_cycles([], [], axs2{:}, [], 512)
+cs_exps{2}.plot_all_cycles([], [], axs2{:}, [], 512)
+end
+X=0;
+for k=1:10
+idx = cs_exps{1}.idx_state_s.scan{9*k};
+% idx = idx-10:idx(end);
+t = AFM.Ts*idx;
+figure(10); 
+xx = cs_exps{1}.x(idx);
+plot(t, xx)
+
+figure(11)
+plot(t, detrend(xx)*512)
+
+[X_, f] = power_spectrum(detrend(xx), AFM.Ts);
+X = X + X_;
+end
+
+figure(12)
+semilogx(f, 10*log10(X/k))
